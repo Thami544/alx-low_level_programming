cef9becb21e3a93dcd3bcdf9124b5d340c12066b
Thami544 <thamidlamini544@gmail.com>
2022-10-13 13:40:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1,"And that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+	return (1);
+
+}
